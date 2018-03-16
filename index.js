8788931c9d20be9da0816792dc0783dbe097c4de
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-16 19:24:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ function whisper(quiet) {
   return quiet.toLowerCase();
 }
 
+function logShout(scream) {
+  console.log(scream.toUpperCase());
+}
+
 function logWhisper(quiet) {
   console.log(quiet.toLowerCase());
 }
 
-
+function sayHiToGrandma(cantHear) {
+  if (cantHear == cantHear.toLowerCase) {
+    console.log("I can't hear you!");
+  }
+}
 
 shout("hello");
 whisper("HELLO")
+logShout("hello");
 logWhisper("HELLO");
