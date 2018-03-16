07ee542692daee23b8e7e5652aa07c9c0890514a
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-16 19:20:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ function shout(scream) {
   return scream.toUpperCase();
 }
 
+function whisper(quiet) {
+  return quiet.toLowerCase();
+}
+
+function () {
+  
+}
+
 shout("hello");
+whisper("HELLO")
